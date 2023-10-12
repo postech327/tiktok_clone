9964d06fd27762bbdd3aa3891aefbb773af89b24
postech327 <dkny719@gmail.com>
2023-10-12 13:22:43 +0900

    6.5 Custom NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
-import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
