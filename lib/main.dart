617f51f604246acd072409ae7dc7661e04a7eca8
postech327 <dkny719@gmail.com>
2023-12-13 11:59:40 +0900

    7.3 Video Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
