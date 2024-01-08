a90a89405018737931dbe13b6ef224628c57ca28
postech327 <dkny719@gmail.com>
2024-01-08 18:23:19 +0900

    12.1 CustomScrollView

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -10,6 +10,29 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          pinned: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              "C:\\Users\\Administrator\\Documents\\tiktok_clone\\lib\\features\\assets\\images\\siha_test.jpg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello!"), //타이틀을 여기다 놓으면 배경안에 있는 제목이 됨!!
+          ),
+        ),
+      ],
+    );
   }
 }
