93a5e908223ab28685994564073d1e8caf8cd842
postech327 <dkny719@gmail.com>
2024-03-05 11:57:07 +0900

    16.2 l10n

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
-import 'package:tiktok_clone/features/settings/settings_screen.dart';
+import "package:flutter_gen/gen_l10n/intl_generated.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,6 +25,7 @@ class TikTokApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       localizationsDelegates: const [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
